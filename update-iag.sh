328de6cecf1fd875883f934c9e4e603c33bcc40a
hemantksingh <hemantksingh.hk@gmail.com>
2016-12-01 18:04:33 +0000

    #636 Build webapp.

--- update-iag.sh
+++ update-iag.sh
@@ -1,3 +1,9 @@
 git clone git@github.com:smbc-digital/iag-webapp.git
 git clone git@github.com:smbc-digital/iag-contentapi.git
 git clone git@github.com:smbc-digital/aws-provisioning.git
+
+pushd iag-webapp
+make build
+export APP_VERSION=1.1.$SNAP_PIPELINE_COUNTER
+make tag
+popd
