541c461c6e6da782947cea73ac4f624291ffbf32
hemantksingh <hemantksingh.hk@gmail.com>
2016-12-02 12:54:13 +0000

    #655 Enable build and publish for contentapi.

--- update-iag.sh
+++ update-iag.sh
@@ -2,8 +2,12 @@ git clone git@github.com:smbc-digital/iag-webapp.git
 git clone git@github.com:smbc-digital/iag-contentapi.git
 git clone git@github.com:smbc-digital/aws-provisioning.git
 
-pushd iag-webapp
-make build
 export APP_VERSION=1.1.$SNAP_PIPELINE_COUNTER
-make tag
+
+pushd iag-webapp
+make package
+popd
+
+pushd iag-contentapi
+make package
 popd
